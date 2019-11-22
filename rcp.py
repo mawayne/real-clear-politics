5c0cc780b4e722a19c2e6cec59ec91e90839bde4
Matt Wayne <matt@thinkdolabs.com>
2019-11-22 16:27:48 -0500

    Achieved 'proper' response - need to input correct data.

--- rcp.py
+++ rcp.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask import url_for
 from flask import jsonify
+import json
 
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return ('Hello, Biatch!')
 
 @app.route('/headlines')
-def headlines():
-    with open('headlines.json') as f:
-        return jsonify(dict, status=200) 
-    # "<a href=%s>file</a>" % url_for('static', filename='headlines.json')
+def headlines_api():
+       headlines = 'headlines.json'
+       return jsonify([headline for headline in headlines])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-source_url = 'http://127.0.0.1:5000/static/headlines.json'
+source_url = 'http://127.0.0.1:5000/headlines'
 source_response = requests.get(source_url)
 source_content = source_response.text
 source_content_list = json.loads(source_content)
 
-print(source_content_list[1]['Title'])
+print(type(source_content))
