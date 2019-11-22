82ec4b9bec113503ff3c869abafd30262c62b74b
Matt Wayne <matt@thinkdolabs.com>
2019-11-22 01:33:04 -0500

    Honestly, I don't know what I did but I think headlines is now returning JSON?

--- rcp.py
+++ rcp.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask import url_for
+from flask import jsonify
 
-app = Flask(__name__, static_url_path='/Users/matthewwayne/dev/rcpolitics/static')
 
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
     return ('Hello, Biatch!')
 
-@app.route('/send')
-def send():
-    return "<a href=%s>file</a>" % url_for('static', filename='headlines.json')
+@app.route('/headlines')
+def headlines():
+    with open('headlines.json') as f:
+        return jsonify(dict, status=200) 
+    # "<a href=%s>file</a>" % url_for('static', filename='headlines.json')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
