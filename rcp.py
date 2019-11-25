09b060c5735f4c9f25d4da00b1ad99f6405e3c1c
Matt Wayne <matt@thinkdolabs.com>
2019-11-25 03:53:23 -0500

    Made two separate files for each method. Main file is rcp.py.

---
+++ rcp.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return ('Hello, Biatch!')
+
+@app.route('/headlines')
+def send_headlines():
+    data_file = 'headlines.json'
+    with open(data_file, 'r') as f:
+        headlines = json.load(f)
+        return jsonify([headline for headline in headlines])
+
+if __name__ == '__main__':
+       app.run(debug=True)
--- rcp_app.py
+++ rcp_app.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, send_from_directory
 
-app = Flask(__name__, static_url_path='/headlines' )
+app = Flask(__name__, static_url_path='/headlines')
 
 @app.route('/')
 def home():
     return ('Hello, Biatch!')
 
 @app.route('/headlines/<path:headlines.json>')
-def send_headlines(path):
-       headlines = send_from_directory('/rcpolitics', 'headlines.json', as_attachment = True)
+def send_headlines(filename):
+       headlines = send_from_directory('/rcpolitics', 'headlines.json')
        return jsonify([headline for headline in headlines])
 
 if __name__ == '__main__':
