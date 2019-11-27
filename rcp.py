7e926fda1dc022c88dee686822786d479be644cb
Matt Wayne <matt@thinkdolabs.com>
2019-11-27 03:24:33 -0800

    Figure out filters.

--- rcp.py
+++ rcp.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, json
+import requests 
+import json
 
 app = Flask(__name__)
 
@@ -12,6 +14,16 @@ def send_headlines():
     with open(data_file) as f:
         headlines = json.load(f)
         return jsonify(headlines)
+
+url = 'http://127.0.0.1:5000/headlines/country'
+headers = {'Content-Type': 'application/json'}
+
+filters = [dict(name='Title', op='==', val='%Syria%')]
+params = dict(q=json.dumps(dict(filters=filters)))
+
+response = requests.get(url, params=params, headers=headers)
+assert response.status_code == 200
+print(response.json())
 
 if __name__ == '__main__':
        app.run(debug=True)
